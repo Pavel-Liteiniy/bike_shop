aaea1b3917aa760c355e3582a664209115ecbcf7
Pavel <paulsev96@gmail.com>
2020-02-16 19:00:59 +0300

    Актуализирует папку build

---
+++ build/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var header = document.querySelector('.header__inner');
+
+header.classList.remove('header__inner--no-js');
+
+var openButton = document.querySelector('.header__button--open');
+var closeButton = document.querySelector('.header__button--close');
+var modal = document.querySelector('.header__wrapper');
+
+modal.classList.remove('header__wrapper--open');
+modal.classList.add('header__wrapper--close');
+
+openButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  modal.classList.add('header__wrapper--open');
+  modal.classList.remove('header__wrapper--close');
+});
+
+closeButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  modal.classList.add('header__wrapper--close');
+  modal.classList.remove('header__wrapper--open');
+});
