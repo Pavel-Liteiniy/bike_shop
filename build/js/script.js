b6be7de8f62df06d969d9334ff8c78738a36f836
Pavel <paulsev96@gmail.com>
2020-04-04 18:24:24 +0300

    Обновляет папку build

--- build/js/script.js
+++ build/js/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
+// отключает классы для работы сайта без js
+
 var header = document.querySelector('.header__inner');
+var settings = document.querySelector('.settings');
 
 header.classList.remove('header__inner--no-js');
+settings.classList.remove('settings--no-js');
+
+// открывает-закрвывает меню в шапке
 
 var openButton = document.querySelector('.header__button--open');
 var closeButton = document.querySelector('.header__button--close');
@@ -25,4 +31,21 @@ closeButton.addEventListener('click', function (evt) {
   modal.classList.add('header__wrapper--close');
   modal.classList.remove('header__wrapper--open');
   document.querySelector('.page').style.overflow = 'visible';
+});
+
+// открывает-закрвывает блок с фильтрами в каталоге
+
+var openSettings = document.querySelector('.filters__open');
+var closeSettings = document.querySelector('.settings__button-close');
+
+openSettings.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  settings.classList.remove('settings--hide');
+});
+
+closeSettings.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  settings.classList.add('settings--hide');
 });
