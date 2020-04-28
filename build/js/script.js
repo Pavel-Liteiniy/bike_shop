273ade2e95083559d8884f9a072ef5f438b09400
Pavel <paulsev96@gmail.com>
2020-04-28 22:59:14 +0300

    Обновляет папку build

--- build/js/script.js
+++ build/js/script.js
@@ -5,8 +5,13 @@
 var header = document.querySelector('.header__inner');
 var settings = document.querySelector('.settings');
 
-header.classList.remove('header__inner--no-js');
-settings.classList.remove('settings--no-js');
+if (header) {
+  header.classList.remove('header__inner--no-js');
+}
+
+if (settings) {
+  settings.classList.remove('settings--no-js');
+}
 
 // открывает-закрвывает меню в шапке
 
@@ -38,14 +43,142 @@ closeButton.addEventListener('click', function (evt) {
 var openSettings = document.querySelector('.filters__open');
 var closeSettings = document.querySelector('.settings__button-close');
 
-openSettings.addEventListener('click', function (evt) {
-  evt.preventDefault();
+if (settings) {
+  openSettings.addEventListener('click', function (evt) {
+    evt.preventDefault();
 
-  settings.classList.remove('settings--hide');
-});
+    settings.classList.remove('settings--hide');
+  });
 
-closeSettings.addEventListener('click', function (evt) {
-  evt.preventDefault();
+  closeSettings.addEventListener('click', function (evt) {
+    evt.preventDefault();
 
-  settings.classList.add('settings--hide');
-});
+    settings.classList.add('settings--hide');
+  });
+}
+
+// сворачивает/разворачивает чекбоксы из блока фильтров
+
+var filterWrappers = document.querySelectorAll('.settings__wrapper');
+var filterButtons = document.querySelectorAll('.settings__button');
+
+var wrapFilters = function () {
+  for (var i = 0; i < filterButtons.length; ++i) {
+    filterButtons[i].onclick = function () {
+      filterButtons[i].classList.toggle('settings__button--open');
+
+      for (var j = 0; i < filterWrappers.length; ++i) {
+        if (filterButtons[i].dataset.buttonFilters === filterWrappers[j].dataset.wrapperFilters) {
+          filterWrappers[j].classList.toggle('settings__wrapper--hide');
+        }
+      }
+    };
+  }
+};
+
+if (filterWrappers) {
+  wrapFilters();
+}
+
+// слайдер на карточке товара для переключения цветов
+
+var productColorImages = document.querySelectorAll('.product-card__image-item');
+var productColorButtons = document.querySelectorAll('.product-card__color-button');
+var productColorButtonChecked = productColorButtons[0];
+
+var switchColors = function () {
+  for (var i = 0; i < productColorButtons.length; ++i) {
+    if (productColorButtons[i].checked) {
+      productColorButtonChecked = productColorButtons[i];
+    }
+
+    productColorButtons[i].onclick = function () {
+      for (var j = 0; i < productColorImages.length; ++j) {
+        if (productColorButtons[i].dataset.productColorButton === productColorImages[j].dataset.productColorImage) {
+          productColorImages[j].style.display = 'block';
+        } else {
+          productColorImages[j].style.display = 'none';
+        }
+      }
+    };
+  }
+};
+
+var switchCheckedColor = function () {
+  for (var j = 0; j < productColorImages.length; ++j) {
+    if (productColorButtonChecked.dataset.productColorButton === productColorImages[j].dataset.productColorImage) {
+      productColorImages[j].style.display = 'block';
+    } else {
+      productColorImages[j].style.display = 'none';
+    }
+  }
+};
+
+if (productColorImages) {
+  switchColors();
+  switchCheckedColor();
+}
+
+
+// переключение в десктопной версии вкладок с детализированной информацией по товару
+
+var tabButtons = document.querySelectorAll('.product-summary__tabs-button');
+var tabBlocks = document.querySelectorAll('.product-summary__item');
+
+var openBlocks = function () {
+  for (var i = 0; i < tabButtons.length; ++i) {
+    tabButtons[i].onclick = function () {
+      for (var j = 0; j < tabBlocks.length; ++j) {
+        var wrapper = tabBlocks[j].querySelector('.product-summary__item-wrapper');
+        if (tabButtons[i].dataset.tabLink === tabBlocks[j].dataset.tabBlock) {
+          tabBlocks[j].classList.add('.product-summary__item--open');
+          wrapper.classList.add('product-summary__item-wrapper--open');
+        } else {
+          tabBlocks[j].classList.remove('.product-summary__item--open');
+          wrapper.classList.remove('product-summary__item-wrapper--open');
+        }
+      }
+    };
+
+    if (tabButtons[i].checked) {
+      for (var j = 0; j < tabBlocks.length; ++j) {
+        var wrapper = tabBlocks[j].querySelector('.product-summary__item-wrapper');
+
+        if (tabButtons[i].dataset.tabLink !== tabBlocks[j].dataset.tabBlock) {
+          wrapper.classList.remove('product-summary__item-wrapper--open');
+        } else {
+          wrapper.classList.add('product-summary__item-wrapper--open');
+        }
+      }
+    }
+  }
+};
+
+if (tabButtons && (document.body.clientWidth >= 768)) {
+  openBlocks();
+}
+
+// Открытие и закрытие вкладок в мобильной версии вкладок с детализированной информацией по товару
+
+var tabLinks = document.querySelectorAll('.product-summary__item-heading a');
+
+var openMobBlocks = function () {
+  for (var i = 0; i < tabLinks.length; ++i) {
+    tabLinks[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      for (var j = 0; j < tabBlocks; ++j) {
+        var wrapper = tabBlocks[j].querySelector('.product-summary__item-wrapper');
+
+        if (tabLinks[i].dataset.tabLink === tabBlocks[j].dataset.tabBlock) {
+          tabBlocks[j].classList.toggle('product-summary__item--open');
+          wrapper.classList.toggle('product-summary__item-wrapper--open');
+        }
+      }
+    });
+  }
+};
+
+if (tabLinks && (document.body.clientWidth < 768)) {
+  openMobBlocks();
+}
